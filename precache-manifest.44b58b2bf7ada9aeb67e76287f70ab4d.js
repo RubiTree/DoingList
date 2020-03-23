3e3e57b5a1248a55c1f1972e165316ced6926ddf
xiongzhengyu <xiongzhengyu@bytedance.com>
2020-03-23 10:41:33 +0800

    ignore

---
+++ precache-manifest.44b58b2bf7ada9aeb67e76287f70ab4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989e9036dafa8550c40baab9b72b7f17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "598bc36673387500e995",
+    "url": "/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "29612877c569ac98f0b5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "598bc36673387500e995",
+    "url": "/static/js/2.a1b2b751.chunk.js"
+  },
+  {
+    "revision": "829759d58d9762f8b3d15b7252f6f2ba",
+    "url": "/static/js/2.a1b2b751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29612877c569ac98f0b5",
+    "url": "/static/js/main.8a70d09f.chunk.js"
+  },
+  {
+    "revision": "0c23392b7fbb363652e8",
+    "url": "/static/js/runtime-main.c5390d91.js"
+  }
+]);
